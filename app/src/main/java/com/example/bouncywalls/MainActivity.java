ddb0f245c5a8f4f2adb6e715e7c219c1c9f57f98
VilkkuKoo <viljamikoivisto27@gmail.com>
2024-11-01 00:28:50 +0200

    i really don't know

---
+++ app/src/main/java/com/example/bouncywalls/MainActivity.java
@@ -0,0 +1,13 @@
+// bouncy-walls/app/src/main/java/com/example/bouncywalls/MainActivity.java
+package com.example.bouncywalls;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
